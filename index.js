45688724feef8a0a5099bfa2d7264a18b2d82067
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-10 08:36:50 -0400

    all tests pass for findMatching funtion

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+//const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+function findMatching(drivers, value) {
+  const listOfName = [];
+  for (const name of drivers) {
+    if (name.toLowerCase() === value.toLowerCase()) {
+      listOfName.push(name);
+    }
+  }
+  return listOfName;
+}
+
+console.log(findMatching(drivers, "Bobby"));
+
+
+
+
+function fuzzyMatch() {
+
+}
+
+function matchName() {
+
+}
