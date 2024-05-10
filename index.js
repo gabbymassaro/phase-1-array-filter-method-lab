35223de5a40c49a7d48a8746d8b7664349b5ea8a
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-10 11:10:19 -0400

    Clean up argument names

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function findMatching(drivers, value) {
+function findMatching(drivers, nameValue1) {
   const listOfName = [];
   for (const name of drivers) {
-    if (name.toLowerCase() === value.toLowerCase()) {
+    if (name.toLowerCase() === nameValue1.toLowerCase()) {
       listOfName.push(name);
     }
   }
@@ -9,10 +9,10 @@ function findMatching(drivers, value) {
 }
 console.log(findMatching(drivers, "Bobby"));
 
-function fuzzyMatch(drivers, value2) {
+function fuzzyMatch(drivers, nameValue2) {
   const beginsWith = [];
   for (const name of drivers) {
-    if (name.startsWith(value2) === value2.startsWith(value2)) {
+    if (name.startsWith(nameValue2) === nameValue2.startsWith(nameValue2)) {
       beginsWith.push(name);
     }
   }
@@ -20,8 +20,8 @@ function fuzzyMatch(drivers, value2) {
 }
 console.log(fuzzyMatch(drivers, "S"));
 
-function matchName(drivers, value3) {
-  return drivers.filter(match => match.name === value3);
+function matchName(drivers, nameValue3) {
+  return drivers.filter(match => match.name === nameValue3);
 }
 const matchingNames = matchName(drivers, "Bobby");
 console.log(matchingNames); 
